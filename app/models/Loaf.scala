88a86ebd7de12bc3b09edcb4fa6234d6eac9c833
Peter Emil Jensen <ubs908@gmail.com>
2017-04-26 12:49:29 +0200

    Changed loaf to encompass correct way of doing case classes

--- app/models/Loaf.scala
+++ app/models/Loaf.scala
@@ -1,8 +1,28 @@
-import scala.collection.mutable
+package models
 
-class Loaf(data: String, timestamp: String = "None", hash: String = "None") {
+import play.api.libs.json._
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
-	val loaf = scala.collection.mutable.Map("data" -> data, "timestamp" -> timestamp, "hash" -> hash);
+import com.roundeights.hasher.Implicits._
+import scala.language.postfixOps
 
-	if (loaf("timestamp") == "None") loaf.update("timestamp", DateTime.now.toString)
+case class Loaf(data: JsValue, timestamp: String, hash: String)
+
+object Loaf {
+  implicit val loafFormat = Json.format[Loaf]
+
+  def generateLoaf(data: JsValue) = {
+    val timestamp: String = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").
+      format(Calendar.getInstance().getTime())
+    val sorted_json = Json.toJson(
+      Map("data" -> "test", "timestamp" -> timestamp)
+    ).as[JsObject]
+    val hash = "test"
+    Loaf(
+      data,
+      timestamp,
+      hash
+    )
+  }
 }
