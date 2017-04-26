89e9a3f8f4bc7e3f0debc8bb20ab9582e77f4577
Peter Emil Jensen <ubs908@gmail.com>
2017-04-26 14:49:43 +0200

    Added validate method and removed printline

--- app/models/Loaf.scala
+++ app/models/Loaf.scala
@@ -8,14 +8,16 @@ import com.roundeights.hasher.Implicits._
 import scala.language.postfixOps
 
 case class Loaf(data: JsValue, timestamp: String, hash: String) {
+
   def calculateHash: String = {
     val stripped_json = JsObject(
       Seq("data" -> data, "hash" -> JsString(hash))
     ) - "hash"
     val sorted_json = JsObject(stripped_json.fields.sortBy(_._1))
-    println(sorted_json.toString)
     sorted_json.toString.sha256
   }
+
+  def validate: Boolean = calculateHash == hash
 
 }
 
@@ -23,7 +25,7 @@ object Loaf {
 
   implicit val loafFormat = Json.format[Loaf]
 
-  def generateLoaf(data: JsValue) = {
+  def generateLoaf(data: JsValue): Loaf = {
     val timestamp: String = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").
       format(Calendar.getInstance().getTime())
     val hash = Loaf(data, timestamp, "").calculateHash
