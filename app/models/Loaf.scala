4813251a4486ba6e45f6b6208e1d9bf0cea0637e
Peter Emil Jensen <ubs908@gmail.com>
2017-04-26 14:37:00 +0200

    Finished Loaf calculateHash

--- app/models/Loaf.scala
+++ app/models/Loaf.scala
@@ -7,18 +7,27 @@ import java.util.Calendar
 import com.roundeights.hasher.Implicits._
 import scala.language.postfixOps
 
-case class Loaf(data: JsValue, timestamp: String, hash: String)
+case class Loaf(data: JsValue, timestamp: String, hash: String) {
+  def calculateHash: String = {
+    val stripped_json = JsObject(
+      Seq("data" -> data, "hash" -> JsString(hash))
+    ) - "hash"
+    val sorted_json = JsObject(stripped_json.fields.sortBy(_._1))
+    println(sorted_json.toString)
+    sorted_json.toString.sha256
+  }
+
+}
 
 object Loaf {
+
   implicit val loafFormat = Json.format[Loaf]
 
   def generateLoaf(data: JsValue) = {
     val timestamp: String = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").
       format(Calendar.getInstance().getTime())
-    val stripped_json = Json.toJson(Loaf(data, timestamp, "")).as[JsObject] - "hash"
-    val sorted_json = JsObject(stripped_json.fields.sortBy(_._1))
-    println(sorted_json.toString)
-    val hash = sorted_json.toString.sha256
+    val hash = Loaf(data, timestamp, "").calculateHash
     Loaf(data, timestamp, hash)
   }
+
 }
