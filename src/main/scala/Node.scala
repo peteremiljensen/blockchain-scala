08d89c576c4b10d3539633b615ea026fe4e85cb7
Peter Emil Jensen <ubs908@gmail.com>
2017-05-01 13:21:18 +0200

    Made a general blocking function for communicating with chainActor and finished methods

--- src/main/scala/Node.scala
+++ src/main/scala/Node.scala
@@ -12,14 +12,21 @@ class Node(port: Int) {
 
   val system = ActorSystem("bc-system")
   val chainActor = system.actorOf(Props[ChainActor], "chain")
-  implicit val duration: Timeout = 20 seconds
+  val timeout = 20 seconds
+  implicit val duration: Timeout = timeout
 
-  def getLength() = {
-    val f = chainActor ? GetLength
-    val result = Await.ready(f, 20 seconds).value.get
+  def addBlock(block: Block) = askWait(AddBlock(block))
+  def getBlock(height: Int) = askWait(GetBlock(height))
+  def getLength = askWait(GetLength)
+  def getChain = askWait(GetChain)
+  def validate = askWait(Validate)
+
+  def askWait(message: Any) = {
+    val f = chainActor ? message
+    val result = Await.ready(f, timeout).value.get
     result match {
-      case Success(length) => Some(length)
-      case Failure(t) => None
+      case Success(data) => Right(data)
+      case Failure(data) => Left(data)
     }
   }
 
